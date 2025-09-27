9d26e04075299a6b5ea98ab42cbe4a36b4d7856e
Mateusz Pietrowcow <104150350+Matian37@users.noreply.github.com>
2025-09-27 18:23:58 +0200

    Update compile.sh
    Co-authored-by: GitHub Actions <github-actions[bot]@users.noreply.github.com>

--- template/compile.sh
+++ template/compile.sh
@@ -2,21 +2,17 @@
 
 if [ "$2" == "fast" ];
 then
-    # 4 seconds
     echo "Fast compilation"
-    g++ -o $1 $1.cpp -Og -ggdb3 -std=c++20
+    g++ -o $1 $1.cpp -O0 -ggdb3 -std=c++20
     exit 0
 elif [ "$2" == "opt" ];
 then
-    # 4 seconds
     echo "Optimized compilation"
     g++ -o $1 $1.cpp -O3 -static -std=c++20
     exit 0
 else
     echo "Debug compilation"
-
-    # 6-7 second compliation
-    g++ -o $1 $1.cpp -Og -ggdb3 --std=c++20 \
+    g++ -o $1 $1.cpp -O0 -ggdb3 --std=c++20 \
         -Wall -Wextra -Wshadow -Wconversion \
         -Wfloat-equal -Wfloat-conversion \
         -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_GLIBCXX_SANITIZE_VECTOR -D_GLIBCXX_PEDANTIC \
